42f0fe48620128fdf4370342b051fd5b58cb6ca4
Abdualiyev Daniyal <danialabdualiev@gmail.com>
2025-02-25 15:55:33 +0500

    Fix SessionLocal import issue in auth.py

--- app/database.py
+++ app/database.py
@@ -4,15 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 
-load_dotenv()  # Load environment variables from .env file
+load_dotenv()  # Load envronment variables from .env file
 
 # Determine if running locally or in Render
-ENVIRONMENT = os.getenv("ENVIRONMENT", "local")  # Default to "local"
-
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
 DATABASE_URL = (
-    os.getenv("DATABASE_URL_LOCAL")
-    if ENVIRONMENT == "local"
-    else os.getenv("DATABASE_URL_RENDER")
+    os.getenv("DATABASE_URL_LOCAL") if ENVIRONMENT == "local" else os.getenv("DATABASE_URL_RENDER")
 )
 
 
