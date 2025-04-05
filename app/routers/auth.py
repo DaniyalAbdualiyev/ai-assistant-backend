26c46cdc9fc8ff7ece40df550f1a59ae85897319
Abdualiyev Daniyal <danialabdualiev@gmail.com>
2025-04-05 14:57:54 +0500

    Fix missing Security import in auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Security
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from app.database import SessionLocal
